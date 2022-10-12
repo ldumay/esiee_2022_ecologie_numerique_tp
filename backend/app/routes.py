5cf06bfc1167e2d46002e24941f317a4952412fb
Maxence <maxence.hennekein@orange.fr>
2022-10-12 16:32:56 +0200

    Calcul de temp du cable sur 30 minutes
    avec test augmentation temperature par intensité

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -34,21 +34,34 @@ def post_data():
 
 @app.get("/test")
 def get_test():
-	temp_total = request.args.get('temp_cable', type=int) or 0
+	temp_cable = request.args.get('temp_cable', type=int) or request.form.get('temp_cable', type=int) or 0
+	temp_ambiant = request.args.get('temp_ambiant', type=int) or request.form.get('temp_ambiant', type=int) or 16
+	intensity = request.args.get('intensity', type=int) or request.form.get('intensity', type=int) or 200
+	wind_speed = request.args.get('wind_speed', type=int) or request.form.get('wind_speed', type=int) or 4
+
 	temp_table = []
+
+	# calcul sur les 30 prochaines minutes
+	for minute in range(1, 30):
+		temp_cable = calcul_temp_minute(temp_cable, temp_ambiant, intensity, wind_speed)
+		print({'time': minute, 'temp': temp_cable}, flush=True)
+		temp_table.append(temp_cable)
+		# line test : intensity + 100 par min
+		intensity += 100
+
 	return {"table": temp_table}
 
-def calcul_temp_minute(temperature_cable: int, temperature_ambiant: int, intensity: int, wind_speed: int):
+def calcul_temp_minute(temperature_cable: float, temperature_ambiant: float, intensity: int, wind_speed: float):
 	# Retourne la température du cable dans une simulation de 1 minute aux paramètres donnés
-	temperature = temperature_cable
 	for time in range(1, 60):
-		temperature = calcul_temp(temperature, temperature_ambiant, intensity, wind_speed)
-	return temperature
+		temperature_cable += calcul_temp(temperature_cable, temperature_ambiant, intensity, wind_speed)
+	return temperature_cable
 
-def calcul_temp(temperature_cable: int, temperature_ambiant: int, intensity: int, wind_speed: int):
+def calcul_temp(temperature_cable: float, temperature_ambiant: float, intensity: int, wind_speed: float) -> float:
 	# Retourne le calcul de la température
 	# formule de chauffe du cable toutes les secondes selon les paramètres donnés
 	# temperature_cable doit etre la temperature de la dernière itération de la formule
+	# wind speed en m/s
 	# demander pour code carbon
 	part1 = ((wind_speed * wind_speed) / 1600) * 0.4 - 0.1
 	part2 = (temperature_cable - temperature_ambiant - ((pow(intensity, 1.4) / 73785) * 130))
